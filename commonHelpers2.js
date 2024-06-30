a214a851b16055ef077c9486eb2ed622bb6d7bc5
DmytroK-goit <159040488+DmytroK-goit@users.noreply.github.com>
2024-06-30 12:19:34 +0000

    Deploying to gh-pages from @ DmytroK-goit/goit-js-hw-10@bcfce10e052d4badd4ad3a59a66bb7dd3b5b32e1 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";import"./assets/vendor-77e16229.js";new Promise((e,t)=>{});document.querySelector(".form");const o=document.querySelector('[name="delay"]');o.addEventListener("input",e=>{console.log("Введене значення:",e.target.value)});
+//# sourceMappingURL=commonHelpers2.js.map

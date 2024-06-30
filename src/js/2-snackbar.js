ff5ca6509ac7fb7b763408d97673e08effc63898
Dmytro Kovbasiuk <k0vbasyuk.dim0n@gmail.com>
2024-06-30 15:54:23 +0300

    1

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,14 +1,40 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
-const promise = new Promise((resolve, reject)=> {})
-    
+const makePromise = (stateValue, delayValue) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (stateValue === "fulfilled") {
+                iziToast.show({
+                    backgroundColor: 'rgb(44, 201, 13)',
+                    message: `✅ Fulfilled promise in ${delayValue} ms`
+                });
+                resolve();
+            } else {
+                iziToast.show({
+                    backgroundColor: 'red',
+                    message: `❌ Rejected promise in ${delayValue} ms`
+                });
+                reject();
+            }
+        }, delayValue);
+    });
+  };
 
 
+document.querySelector('.form').addEventListener('submit', function(event) {
+event.preventDefault();
 
-const form = document.querySelector('.form')
-const inputForm = document.querySelector('[name="delay"]')
-inputForm.addEventListener('input', event => {
-    console.log('Введене значення:', event.target.value);
-})
+const form = event.target; 
+const delayValue = form.querySelector('input[name="delay"]').value;
+const stateValue = form.querySelector('input[name="state"]:checked').value;
+
+makePromise(stateValue, delayValue);
+
+
+ });
+
+ 
+
+
 
